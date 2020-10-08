92fab663f085ddb84cb8bc0d7978566065d7b21f
Sushrut Kasture <kasturesushrut@gmail.com>
2020-10-09 00:39:06 +0530

    Revert "Solution for chapter 2"
    This reverts commit 2f00fb1913d898f92fb90b5e176b17d285fad9bb.

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -136,43 +136,43 @@ functions in GHCi and insert the corresponding resulting output below:
 
 List of booleans:
 >>> :t [True, False]
-[True, False] :: [Bool]
+
 
 String is a list of characters:
 >>> :t "some string"
-"some string" :: [Char]
+
 
 Empty list:
 >>> :t []
-[] :: [a]
+
 
 Append two lists:
 >>> :t (++)
-(++) :: [a] -> [a] -> [a]
+
 
 Prepend an element at the beginning of a list:
 >>> :t (:)
-(:) :: a -> [a] -> [a]
+
 
 Reverse a list:
 >>> :t reverse
-reverse :: [a] -> [a]
+
 
 Take first N elements of a list:
 >>> :t take
-take :: Int -> [a] -> [a]
+
 
 Create list from N same elements:
 >>> :t replicate
-replicate :: Int -> a -> [a]
+
 
 Split a string by line breaks:
 >>> :t lines
-lines :: String -> [String]
+
 
 Join a list of strings with line breaks:
 >>> :t unlines
-unlines :: [String] -> String
+
 
 -}
 
@@ -186,19 +186,19 @@ Evaluate the following expressions in GHCi and insert the answers. Try
 to guess first, what you will see.
 
 >>> [10, 2] ++ [3, 1, 5]
-[10,2,3,1,5]
+
 >>> [] ++ [1, 4]  -- [] is an empty list
-[1,4]
+
 >>> 3 : [1, 2]
-[3,1,2]
+
 >>> 4 : 2 : [5, 10]  -- prepend multiple elements
-[4,2,5,10]
+
 >>> [1 .. 10]  -- list ranges
-[1,2,3,4,5,6,7,8,9,10]
+
 >>> [10 .. 1]
-[]
+
 >>> [10, 9 .. 1]  -- backwards list with explicit step
-[10,9,8,7,6,5,4,3,2,1]
+
 >>> length [4, 10, 5]  -- list length
 
 >>> replicate 5 True
@@ -332,17 +332,11 @@ yummy functions.
 
 Don't forget that you can load this module in GHCi to call functions
 from it!
-    
-3 5 0,1,2,3,4,5,6,7
 
 ghci> :l src/Chapter2.hs
 -}
 subList :: Int -> Int -> [a] -> [a]
-subList a b l
- | a < 0 || b < 0 = []
- | a > b = []
- | (length l) < a = []
- | otherwise = take (b-a+1) (drop a l)
+subList = error "subList: Not implemented!"
 
 {- |
 =⚔️= Task 4
@@ -355,9 +349,7 @@ Implement a function that returns only the first half of a given list.
 "b"
 -}
 -- PUT THE FUNCTION TYPE IN HERE
-firstHalf :: [a] -> [a]
-firstHalf l = take h l 
-  where h = div (length l) 2
+firstHalf l = error "firstHalf: Not implemented!"
 
 
 {- |
@@ -508,9 +500,7 @@ True
 >>> isThird42 [42, 42, 0, 42]
 False
 -}
-isThird42 :: [Int] -> Bool
-isThird42 (_:_:42:_) = True
-isThird42 _ = False
+isThird42 = error "isThird42: Not implemented!"
 
 
 {- |
@@ -615,8 +605,7 @@ Implement a function that duplicates each element of the list
 
 -}
 duplicate :: [a] -> [a]
-duplicate [] = []
-duplicate (x:xs) = x:x:duplicate xs
+duplicate = error "duplicate: Not implemented!"
 
 
 {- |
@@ -631,10 +620,7 @@ Write a function that takes elements of a list only on even positions.
 >>> takeEven [2, 1, 3, 5, 4]
 [2,3,4]
 -}
-takeEven :: [a] -> [a]
-takeEven [] = []
-takeEven (a:b:rest) = a:takeEven rest
-takeEven (a:rest) = [a] 
+takeEven = error "takeEven: Not implemented!"
 
 {- |
 =🛡= Higher-order functions
@@ -741,8 +727,7 @@ value of the element itself
 🕯 HINT: Use combination of 'map' and 'replicate'
 -}
 smartReplicate :: [Int] -> [Int]
-smartReplicate [] = []
-smartReplicate (x:xs) = replicate x x ++ smartReplicate xs
+smartReplicate l = error "smartReplicate: Not implemented!"
 
 {- |
 =⚔️= Task 9
@@ -755,8 +740,7 @@ the list with only those lists that contain a passed element.
 
 🕯 HINT: Use the 'elem' function to check whether an element belongs to a list
 -}
-contains :: Int -> [[Int]] -> [[Int]]
-contains n l = filter (elem n) l
+contains = error "contains: Not implemented!"
 
 
 {- |
@@ -796,13 +780,13 @@ Let's now try to eta-reduce some of the functions and ensure that we
 mastered the skill of eta-reducing.
 -}
 divideTenBy :: Int -> Int
-divideTenBy = div 10
+divideTenBy x = div 10 x
 
 -- TODO: type ;)
-listElementsLessThan x = filter (< x)
+listElementsLessThan x l = filter (< x) l
 
 -- Can you eta-reduce this one???
-pairMul = zipWith (*)
+pairMul xs ys = zipWith (*) xs ys
 
 {- |
 =🛡= Lazy evaluation
@@ -857,11 +841,7 @@ list.
 
 🕯 HINT: Use the 'cycle' function
 -}
-rotate :: Int -> [a] -> [a]
-rotate c l
- | c < 0 = []
- | length l == 0 = []
- | otherwise = drop c (take (c + length l) (cycle l))
+rotate = error "rotate: Not implemented!"
 
 {- |
 =💣= Task 12*
@@ -877,10 +857,7 @@ and reverses it.
   function, but in this task, you need to implement it manually. No
   cheating!
 -}
-rewind :: [a] -> [a]
-rewind [] = []
-rewind (x:[]) = [x]
-rewind (x:xs) = (rewind xs) ++ [x]
+rewind = error "rewind: Not Implemented!"
 
 
 {-
